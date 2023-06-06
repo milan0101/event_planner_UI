054ce4f3db62974c4fbb5171482719e145e35afd
Milan OS <milan.s@thinkpalm.com>
2023-06-06 12:59:06 +0530

    signi page design changed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,34 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Feed.scss';
+import axios from 'axios';
 
 function Feeds() {
+
+  const [feeds, setFeeds] = useState([]);
+
+  useEffect(() => {
+    console.log(feeds);
+    axios.get('http://localhost:8000/events').then(response => {
+      setFeeds(response.data);
+    }).catch(error => console.log(error));
+  }, []);
   return (
     <div className='fb'>
-       <div className='fcontent'>
-        
-       </div>
-       <div className='fcontent'>
-        
-       </div>
-       <div className='fcontent'>
-        
-       </div>
-       <div className='fcontent'>
-        
-       </div>
-       <div className='fcontent'>
-        
-       </div>
-       <div className='fcontent'>
-        
-       </div>
-       <div className='fcontent'>
-        
-       </div>
-       
-       
+      {
+        feeds && feeds.map((feeds) => (
+          <div key={feeds.id}>
+            <div className='fcontent'>
+              <h2>{feeds.title}</h2>
+            </div>
+
+          </div>
+        ))
+      }
+
     </div>
-   
+
   )
 }
 
